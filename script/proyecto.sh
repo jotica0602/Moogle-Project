ee0a9a0cf3e67142a2244538209d733f9578867d
jotica0602 <alejandroeb5271@gmail.com>
2023-07-23 17:34:33 -0400

    Execute script using functions or go interactive.

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -1,91 +1,186 @@
 #!/bin/bash
-while true; do
-    clear
-    echo "Type the option you want to perform:"
-    echo
-    echo "-run"
-    echo "-report"
-    echo "-slides"
-    echo "-show_report"
-    echo "-show_slides"
-    echo "-clean"
-    echo "-quit"
-    echo
 
-    read option
-    case $option in 
-        run)
-            echo -e "\e[32m Launching Moogle!...\e[0m"
-            cd ..
-            dotnet watch run --project MoogleServer
-            echo
-            echo
-            echo "Press Enter to continue" 
-            cd script
-            read
-            ;;
-        report)
-            echo -e "\e[32m Compiling Report...\e[0m"
-            cd .. 
-            cd Informe
-            latexmk -pdf Informe.tex
-            echo "Press Enter to continue"
-            cd ..
-            cd script
-            read
-            ;;
-        slides) 
-            echo -e "\e[32m Compiling Slides\e[0m"
-            cd ..
-            cd Presentacion
-            cd ..
-            cd script
-            echo "Press Enter to continue"
-            read
-            ;;	
-        show_report)
-            echo -e "\e[32m Showing Report...\e[0m"
-            cd ..
-            cd Informe
-            xdg-open Informe.pdf
-            start Informe.pdf
-            cd ..
-            cd script
-            echo "Press Enter to continue"
-            read
-            ;;
-        show_slides)
-            echo -e "\e[32m Showing Slides...\e[0m"
-            cd ..
-            cd Presentacion
-            #xdg-open Presentacion
-            cd ..
-            cd script
-            echo "Press Enter to continue"
-            read
-            ;;
-        clean)
-            echo -e "\e[32m Cleaning Temporal Files...\e[0m"
-            cd ..
-            cd Informe
-            rm Informe.aux Informe.fdb_latexmk Informe.fls Informe.log
-            cd ..
-            cd Presentacion
-            #Borrar
-            echo "Press Enter to continue"
-            read
-            cd ..
-            cd script
-            ;;
-        quit) 
-            echo -e "\e[32m Closing..."
-            break
-            ;; 
-        *)
-            echo -e "\e[31m WRONG INPUT...\e[0m"
-            read
-    esac
-done
+# Execution Functions
+function run(){
+    echo
+    echo -e "\e[32mLaunching Moogle!...\e[0m"
+    cd ..
+    dotnet watch run --project MoogleServer
+    clear
+    echo -e "\e[31mStopped Moogle! Execution.\e[0m"
+    cd script
+}
+
+function report(){
+    echo
+    echo -e "\e[32mBuilding Report...\e[0m"
+    cd ..
+    cd Informe
+    latexmk -pdf Informe.tex
+    clear
+    echo -e "\e[32mBuilt Report.\e[0m"
+    cd .. 
+    cd script
+}
+
+function slides(){
+    echo
+    echo -e "\e[32mBuilding Slides...\e[0m"
+    cd ..
+    cd Presentacion
+    #compilar presentacion
+    clear
+    echo -e "\e[32mBuilt Slides.\e[0m"
+    cd ..
+    cd script
+}
+
+function show_report(){
+    clear
+    echo
+    echo -e "\e[32mShowing Report...\e[0m"
+    cd .. 
+    cd Informe
+    xdg-open Informe.pdf
+}
+
+function show_slides(){
+    clear
+    echo
+    echo -e "\e[32mShowing Slides...\e[0m"
+    cd ..
+    cd Presentacion
+    #xdg-open Presentacion
+    cd ..
+    cd script
+}
+
+function clean(){
+    echo
+    echo -e "\e[32mCleaning Temporal Files...\e[0m"
+    cd ..
+    cd Informe
+    rm Informe.aux Informe.fdb_latexmk Informe.fls Informe.log
+    cd ..
+    cd Presentacion
+    #rm Borrar temporales de la presentacion
+    clear
+    echo -e "\e[32mTemporal Files Cleaned.\e[0m"
+    cd ..
+    cd script
+
+}
+
+# Interactive Function
+function interactive(){
+    while true; do
+        clear
+        echo "Type the option number you want to perform:"
+        echo
+        echo "1) run              -Launch Moogle!"
+        echo "2) report           -Build Moogle! Report"
+        echo "3) slides           -Build Moogle! Slides" 
+        echo "4) show_report      -Show Moogle! Report"
+        echo "5) show_slides      -Show Moogle! Slides"
+        echo "6) clean            -Clean Temporal Files"
+        echo "7) quit             -Exit"
+        echo "-----------Type Below-----------------------------------"
+
+        read option
+        case $option in 
+            1)
+                run
+                echo -e "\e[32mPress Enter to continue\e[0m"
+                read
+                ;;
+            2)
+                report
+                echo -e "\e[32mPress Enter to continue\e[0m"
+                read
+                ;;
+            3) 
+                slides
+                echo -e "\e[32mPress Enter to continue\e[0m"
+                read
+                ;;	
+            4)
+                show_report
+                start Informe.pdf
+                echo -e "\e[32mPress Enter to continue\e[0m"
+                cd ..
+                cd script
+                read
+                ;;
+            5)
+                show_slides
+                echo -e "\e[32mPress Enter to continue\e[0m"
+                read
+                ;;
+            6)
+                clean
+                echo -e "\e[32mPress Enter to continue\e[0m"
+                read
+                ;;
+            7) 
+                echo -e "\e[31mClosing..."
+                clear
+                echo -e "\e[31mClosed..."
+                break
+                ;; 
+            *)
+                echo -e "\e[31mWRONG INPUT...\e[0m"
+                echo -e "\e[31mPress Enter and type again...\e[0m"
+                read
+        esac
+
+    done
+}
+
+
+FUNCTIONS="run report slides show_report show_slides clean interactive"
+
+execute=$1
+# If there's no function, show available functions
+if [ "$execute" = "" ]; then
+    echo
+    echo "Functions:"
+    for i in $FUNCTIONS; do
+        echo "-$i"
+    done
+    exit 1
+fi
+
+
+case $execute in
+    run)
+        run
+        ;;
+    report)
+        report
+        ;;
+    slides)
+        slides
+        ;;
+    show_report)
+        show_report
+        start Informe.pdf
+        ;;
+    show_slides)
+        show_slides
+        ;;
+    clean)
+        clean
+        ;;
+    interactive)
+        interactive
+        ;;
+    *)
+        echo
+        echo -e "\e[31mWRONG FUNCTION...\e[0m"
+esac
+
+
+
 
 
 
